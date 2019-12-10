5342a6114b94b59fb67b5eeea5d25fcc88006824
fruizga <ruizgallego@gmail.com>
2019-12-10 17:47:29 -0500

    printing numbers tasks 4 & 5

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+i = 1
+while (i < 99):
+    print("{} = {}".format(i, hex(i)))
+    i = i + 1
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = 0
+while i < 100:
+    if i != 99:
+        print("{:02d}, ".format(i), end='')
+    else:
+        print("{:02d}".format(i))
+    i = i + 1
