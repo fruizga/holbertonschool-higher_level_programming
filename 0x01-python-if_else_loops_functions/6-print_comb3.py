e1653bb9577924803336aaf0e5471f28d4cc05d0
fruizga <ruizgallego@gmail.com>
2019-12-11 14:51:56 -0500

    fixing coding style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 i = 0
 while i < 10:
-	j = i +1
-	while j < 10:
-	    if i == 8 and j == 9:
-	      print("{:d}{:d}".format(i, j))
-	      break
-	    if i < j:
-	        print("{:d}{:d}, ".format(i, j), end='')
-	    j = j + 1
-	i = i + 1
+    j = i + 1
+    while j < 10:
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+            break
+        if i < j:
+            print("{:d}{:d}, ".format(i, j), end='')
+        j = j + 1
+    i = i + 1
