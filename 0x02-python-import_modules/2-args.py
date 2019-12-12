5de24ac1fb01b2d83ca0373a715f489a14b4f998
fruizga <ruizgallego@gmail.com>
2019-12-12 17:48:32 -0500

    change output message

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,10 @@
 import sys
 end = len(sys.argv)
 i = 1
-print("{:d} arguments:".format(end - 1))
-for i in range(i, end):
+if end == 2:
+    print("{:d} argument:".format(end - 1))
     print("{:d}: {:s}".format(i, sys.argv[i]))
+else:
+    print("{:d} arguments:".format(end - 1))
+    for i in range(i, end):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
