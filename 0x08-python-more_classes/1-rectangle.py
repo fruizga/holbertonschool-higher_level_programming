3050d11bd8a3077d6ee3cb2553861b3477761e7e
fruizga <ruizgallego@gmail.com>
2020-01-27 10:55:59 -0500

    pep8 retangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,23 +13,22 @@ class Rectangle:
             raise Exception("size must be an integer")
         if height < 0:
             raise Exception("size must be >= 0")
-        
-        
+
     @property
     def width(self):
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         if type(value) != int:
             raise Exception("size must be an integer")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
-    
-    @height.setter    
+
+    @height.setter
     def height(self, value):
         if type(value) != int:
             raise Exception("size must be an integer")
