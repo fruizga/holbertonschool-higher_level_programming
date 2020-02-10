7fffb6287ba256cc2b151570545d92e9097dd2c9
fruizga <ruizgallego@gmail.com>
2020-02-10 12:15:13 -0500

    REctangles and squares

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class BaseGeometry.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+Rectangle that inherits from BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
