f75f73f12387dcfa3f5caec53bc6f279766cc836
fruizga <ruizgallego@gmail.com>
2020-02-26 14:50:38 -0500

    Rect angle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,5 +9,42 @@ Base = __import__('base').Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
         self.__width = width
         self.__height = height
+        self.x = x
+        self.y = y
+                
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.height
+    
+    @height.setter
+    def height(value):
+        self.height = value
+    
+    @property
+    def x(self):
+        return self.x
+    
+    @x.setter
+    def x(value):
+        self.x = value
+    
+    @property
+    def y(self):
+        return self.y
+    
+    @y.setter
+    def y(value):
+        self.y = value
+
+        
