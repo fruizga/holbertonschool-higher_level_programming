9431f27a9babb127f4d49b4371d30bf9a213c698
fruizga <ruizgallego@gmail.com>
2020-03-11 00:11:44 -0500

    list shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,2 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows.
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
