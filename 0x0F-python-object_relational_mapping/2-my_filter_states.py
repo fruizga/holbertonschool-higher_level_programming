a0ad84228c150dec580aa8dedfb1c4a33e2cd712
fruizga <ruizgallego@gmail.com>
2020-04-08 23:11:40 -0500

    SQL Injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost\
     ", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cur = mydb.cursor()
+    cur = db.cursor()
     cur.execute("""SELECT * from states WHERE name LIKE BINARY '{}%'\
                 ORDER BY id ASC""".format(argv[4]))
     rows = cur.fetchall()
