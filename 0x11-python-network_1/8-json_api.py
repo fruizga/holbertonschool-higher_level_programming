10c69db0bd80bb80bf5fcde929737042313b1e6d
fruizga <ruizgallego@gmail.com>
2020-04-20 00:03:07 -0500

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """ takes in a letter and sends a POST with the letter as a parameter """
-from sys import argv
+import sys
 import requests
 
 if __name__ == "__main__":
-    a = argv[1] if len(argv) > 1 else ""
     try:
-        req = requests.post('http://0.0.0.0:5000/search_user\
-        ', data={'a': a}).json()
-        print("[{}] {}".format(req['id'], R1['name'])
-              if 'id' in req and 'name' in req else "No result")
-    except ValueError:
-        print("Not a valid JSON")
+        q = argv[1]
+    except:
+        q = ''
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+    r = requests.post(url, payload)
+    try:
+        json = r.json()
+        if len(json) == 0:
+            print('No result')
+        else:
+            json_id = json.get('id')
+            json_name = json.get('name')
+            print('[{}] {}'.format(json_id, json_name))
+    except:
+        print('Not a valid JSON')
