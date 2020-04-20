39456938a948c1fb56162aa635fa1f6c82d21d0d
fruizga <ruizgallego@gmail.com>
2020-04-19 20:57:53 -0500

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers """
+
+
+def find_peak(list_of_integers):
+    """ searching the peak """
+    arr = list_of_integers
+    if not arr:
+        return None
+    if len(arr) > 1:
+        if arr[0] >= arr[1]:
+            return arr[0]
+        if arr[-1] >= arr[-2]:
+            return arr[-1]
+        for i in range(len(arr)):
+            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                return arr[i]
+    return arr[0]
