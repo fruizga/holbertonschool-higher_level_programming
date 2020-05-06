03361f4f0c8d7928a222832dbc6b3affd83fb6ba
fruizga <ruizgallego@gmail.com>
2020-05-06 12:20:27 -0500

    filter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
+"""
+takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+"""
 import MySQLdb
 from sys import argv
 
-
-if __name__ == '__main__':
-
-    a_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-    curs = a_db.cursor()
-    curs.execute("SELECT * FROM cities\
-                JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s\
-                ORDER BY cities.id ASC", (argv[4], ))
-    states = curs.fetchall()
-    all_cities = []
-    for records in states:
-        if records[4] == argv[4]:
-            all_cities.append(records[2])
-    print(', '.join(all_cities))
-    curs.close()
-    a_db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+    INNER JOIN states ON cities.state_id = states.id\
+    WHERE states.name =%s\
+    ORDER BY cities.id ASC", (argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
